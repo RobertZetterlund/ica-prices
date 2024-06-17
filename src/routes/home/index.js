2475fe58a9f4f6032343ffe398029ce644a35ea6
Robert Zetterlund <robert.zetterlund@outlook.com>
2024-06-17 12:13:26 +0200

    Update preload, seo, add share button

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,9 +1,38 @@
 import { h } from "preact";
-import { useCallback, useEffect, useMemo, useState } from "preact/hooks";
+import {
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState,
+} from "preact/hooks";
 
 import games from "../../assets/games.json";
 
 const GUESSES = [0, 1, 2, 3, 4];
+
+const GUESS_COLOR_EMOJI = {
+  red: "🟥",
+  orange: "🟧",
+  green: "✅",
+};
+
+const PROXIMITY_EMOJI = {
+  correct: "",
+  above: "⬇️",
+  below: "⬆️",
+};
+
+// Ica-Priset #1 3/5 ⬇️🟥 ⬇️🟥 ✅ https://ica-prices.vercel.app/
+function createSharableLink(guesses, day) {
+  return `ICA-priset #${day} ${guesses.length}/5 ${guesses
+    .map((guess) => {
+      return `${
+        PROXIMITY_EMOJI[guess.proximity] + GUESS_COLOR_EMOJI[guess.color]
+      }`;
+    })
+    .join(" ")} ${window.location.href}`;
+}
 
 function Home() {
   const dayId = useMemo(() => {
@@ -22,11 +51,28 @@ function Home() {
   const game = games[gameId];
   const [guesses, setGuesses] = useState([]);
 
+  const [showToast, setShowToast] = useState(false);
+  const toastTimeoutRef = useRef();
+
+  const handleClickShare = useCallback(() => {
+    const link = createSharableLink(guesses, dayId);
+    navigator.clipboard?.writeText(link);
+    setShowToast(true);
+    clearTimeout(toastTimeoutRef.current);
+    toastTimeoutRef.current = setTimeout(() => {
+      setShowToast(false);
+    }, 2000);
+  }, [guesses, dayId]);
+
   useEffect(() => {
     setGuesses(JSON.parse(localStorage.getItem(gameId) ?? "[]"));
   }, [gameId]);
 
   const [input, setInput] = useState("");
+  const numericInput = useMemo(() => {
+    const normalized = input.replace(/[^0-9.]/, ".");
+    return Number(normalized);
+  }, [input]);
 
   const performGuess = useCallback(
     (guessValue) => {
@@ -80,6 +126,9 @@ function Home() {
 
   return (
     <>
+      <div className={`share-toast ${showToast ? "show" : ""}`}>
+        ✓ Kopierat till Urklipp!
+      </div>
       <header>
         <h1>
           <svg viewBox="0 7.77 32 16.449" width="48px">
@@ -105,6 +154,22 @@ function Home() {
               : ""}
           </span>
           <h2>{game_fin && `Pris: ${game.price} kr`}</h2>
+          {game_fin && (
+            <button className="copy-result" onClick={handleClickShare}>
+              Kopiera resultat
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 50 50"
+                width="24px"
+                height="24px"
+                fill="currentColor"
+              >
+                <path d="M30.3 13.7 25 8.4l-5.3 5.3-1.4-1.4L25 5.6l6.7 6.7z" />
+                <path d="M24 7h2v21h-2z" />
+                <path d="M35 40H15c-1.7 0-3-1.3-3-3V19c0-1.7 1.3-3 3-3h7v2h-7c-.6 0-1 .4-1 1v18c0 .6.4 1 1 1h20c.6 0 1-.4 1-1V19c0-.6-.4-1-1-1h-7v-2h7c1.7 0 3 1.3 3 3v18c0 1.7-1.3 3-3 3z" />
+              </svg>
+            </button>
+          )}
         </div>
         <div className="content">
           <div className="guesses">
@@ -115,13 +180,13 @@ function Home() {
           <form
             onSubmit={(e) => {
               e.preventDefault();
-              performGuess(input);
+              performGuess(numericInput);
             }}
           >
             <div className="price-input">
               <input
+                type={isMobile ? undefined : "number"}
                 disabled={game_fin}
-                type="number"
                 value={input}
                 onInput={(e) => {
                   setInput(e.currentTarget.value);
@@ -134,7 +199,10 @@ function Home() {
               />
               <span className="currency">kr</span>
             </div>
-            <StampButton disabled={game_fin || input === ""} text="Gissa" />
+            <StampButton
+              disabled={game_fin || numericInput === NaN || !numericInput}
+              text="Gissa"
+            />
           </form>
         </div>
       </main>
@@ -192,3 +260,21 @@ function StampButton({ disabled, text }) {
   );
 }
 export default Home;
+
+// iPhone with comma instead of dot causes headache, lets ignore it and put
+// this piece of bandage on it
+const isMobile = (function (a) {
+  return !!(
+    /(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(
+      a
+    ) ||
+    /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(
+      a.substr(0, 4)
+    )
+  );
+})(
+  (typeof navigator !== "undefined" &&
+    (navigator?.userAgent || navigator?.vendor)) ||
+    (typeof window !== "undefined" && window?.opera) ||
+    ""
+);
